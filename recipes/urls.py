03a4769aa49da5a37ec3a8671ee1840e30bb67e2
Emily Rosenberg <earosenberg@gmail.com>
2024-01-16 18:14:59 +0100

    Add recipe detail template to show a single recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddRecipe, Recipes
+from .views import AddRecipe, Recipes, RecipeDetail
 
 
 urlpatterns = [
     path("add/", AddRecipe.as_view(), name="add_recipe"),
     path("recipes/", Recipes.as_view(), name="recipes"),
+    path("<slug:pk>/", RecipeDetail.as_view(), name="recipe_detail"),
 ]
