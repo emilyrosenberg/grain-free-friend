ddebed7253310c9ee349b6aff06f211e97f5d3be
Emily Rosenberg <earosenberg@gmail.com>
2024-01-20 14:57:03 +0100

    Add filter function to admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -4,6 +4,9 @@ from .models import Recipe
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
+
+    list_filter = (['posted_date'])
+
     list_display = (
         "title",
         "instructions",
