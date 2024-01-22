0af5e80414870193f037222518d2ca50117218f5
Emily Rosenberg <earosenberg@gmail.com>
2024-01-22 19:04:26 +0100

    Update allowed hosts 2

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['grain-free-friend-d9004ed6c6b5.herokuapp.com']
+ALLOWED_HOSTS = ['grain-free-friend-d9004ed6c6b5.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
