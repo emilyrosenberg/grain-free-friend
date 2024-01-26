1cd5aa7a51c4c0afa41bbf2b0a47a191d43fbab8
Emily Rosenberg <earosenberg@gmail.com>
2024-01-26 13:21:04 +0100

    Add favorite finder to get a random recipe with keyword

--- favorites/urls.py
+++ favorites/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Favorites
+from .views import Favorites, GetOption
 
 
 urlpatterns = [
     path("", Favorites.as_view(), name="favorites"),
+    path("favorite_finder/", GetOption.as_view(), name="favorite_finder"),
 ]
--- favorites/views.py
+++ favorites/views.py
@@ -1,18 +1,54 @@
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from django.db.models import Q
+
 from .models import Option
+from recipes.models import Recipe
+import random
 
 
-class Favorites(LoginRequiredMixin, TemplateView):
+class Favorites(TemplateView):
     """View user's favorites"""
 
     template_name = "favorites/favorites.html"
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self):
         options = Option.objects.filter(user=self.request.user)
-        # Add code for more filters here
 
         context = {"options": options}
 
+        return context
+
+
+class GetOption(LoginRequiredMixin, TemplateView):
+    """Get option based on search queries or empty input"""
+
+    template_name = "favorites/favorite_finder.html"
+
+    def get_context_data(self):
+        query = self.request.GET.get("search")
+
+        if query:
+            recipes = Recipe.objects.filter(
+                Q(title__icontains=query)
+                | Q(description__icontains=query)
+                | Q(ingredients__icontains=query)
+                | Q(instructions__icontains=query)
+                | Q(tag1__icontains=query)
+                | Q(tag2__icontains=query)
+                | Q(tag3__icontains=query)
+            )
+        else:
+            if len(Recipe.objects.all()) < 1:
+                recipes = []
+            else:
+                recipes = Recipe.objects.filter()
+        if len(recipes) > 0:
+            recipe = random.choice(recipes)
+            context = {"recipe": recipe}
+        # These are the same because I'm filtering differently than the tutorial, may cause a problem?
+        else:
+            recipe = random.choice(recipes)
+            context = {"recipe": recipe}
         return context
