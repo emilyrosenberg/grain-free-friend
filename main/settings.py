0e4e52fda9c411db9eb2f2ae5803cdb881521844
Emily Rosenberg <earosenberg@gmail.com>
2024-01-28 11:14:40 +0100

    Set DEBUG = False

--- main/settings.py
+++ main/settings.py
@@ -11,17 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 if os.path.exists("env.py"):
     import env
 
-# if os.path.isfile('env.py'):
-#      import env
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["grain-free-friend-d9004ed6c6b5.herokuapp.com", "127.0.0.1"]
 
